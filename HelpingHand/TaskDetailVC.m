808cf490c78e4e79745f12e640bfe89c8c002d32
j4wang <john@admins-MacBook-Pro.local>
2013-12-07 10:35:28 -0800

    Initial Task Detail VC Check-in

---
+++ HelpingHand/TaskDetailVC.m
@@ -0,0 +1,38 @@
+//
+//  TaskDetailVC.m
+//  HelpingHand
+//
+//  Created by John on 12/7/13.
+//  Copyright (c) 2013 HelpingHand. All rights reserved.
+//
+
+#import "TaskDetailVC.h"
+
+@interface TaskDetailVC ()
+
+@end
+
+@implementation TaskDetailVC
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
