fbf4c30830f88f5ed3179d52473eaceec1293920
j4wang <john@admins-MacBook-Pro.local>
2013-12-07 09:56:52 -0800

    Add Twitter OAuth code

---
+++ HelpingHand/TwitterClient.h
@@ -0,0 +1,21 @@
+//
+//  TwitterClient.h
+//  HelpingHand
+//
+//  Created by John on 12/7/13.
+//  Copyright (c) 2013 HelpingHand. All rights reserved.
+//
+
+#import "AFOAuth1Client.h"
+
+@interface TwitterClient : AFOAuth1Client
+
++ (TwitterClient *)instance;
+
+// Users API
+
+- (void)authorizeWithCallbackUrl:(NSURL *)callbackUrl success:(void (^)(AFOAuth1Token *accessToken, id responseObject))success failure:(void (^)(NSError *error))failure;
+
+- (void)currentUserWithSuccess:(void (^)(AFHTTPRequestOperation *operation, id response))success failure:(void (^)(AFHTTPRequestOperation *operation, NSError *error))failure;
+
+@end
