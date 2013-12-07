cd89f1c3f0d85332a11ccb780279e13f457e6155
imju <imjubyon@gmail.com>
2013-12-06 21:24:07 -0800

    initial checkin

---
+++ HelpingHand/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HelpingHand
+//
+//  Created by bgbb on 12/6/13.
+//  Copyright (c) 2013 HelpingHand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelpingHandTests/HelpingHandTests.m
@@ -0,0 +1,34 @@
+//
+//  HelpingHandTests.m
+//  HelpingHandTests
+//
+//  Created by bgbb on 12/6/13.
+//  Copyright (c) 2013 HelpingHand. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HelpingHandTests : XCTestCase
+
+@end
+
+@implementation HelpingHandTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
