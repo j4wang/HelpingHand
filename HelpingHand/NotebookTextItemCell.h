392b8a414b67698f42f4c7b5129ab7f7f12c0de0
j4wang <john@admins-MacBook-Pro.local>
2013-12-07 11:53:21 -0800

    Initial checkin - Screen 6 cells

---
+++ HelpingHand/NotebookTextItemCell.h
@@ -0,0 +1,13 @@
+//
+//  NotebookItemCell.h
+//  HelpingHand
+//
+//  Created by John on 12/7/13.
+//  Copyright (c) 2013 HelpingHand. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotebookTextItemCell : UITableViewCell
+
+@end
